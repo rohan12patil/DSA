1df40c6c23d1b882159cf7d46c4d02f3abcc38e2
Rohan Patil <rohan12patil@gmail.com>
2023-08-31 15:48:58 +0000

    HashSet - distinct element in Array

--- Hashing/2_count_distinct_elements.js
+++ Hashing/2_count_distinct_elements.js
@@ -30,7 +30,7 @@ For Input 2: The distinct elements of the array are 3, 9, 0 and 1.
 let Ar = [3, 3, 3, 9, 0, 1, 0]
 let N = Ar.length;
 let hM = new Map();
-
+/*
 for(let i=0;i<N;i++){
     if(hM.has(Ar[i])) {
         cnt = hM.get(Ar[i]);
@@ -41,3 +41,12 @@ for(let i=0;i<N;i++){
 }
 console.log("HM:: ",hM);
 console.log("Distinct elements in Array are :: ",hM.size);
+*/
+
+// Todo: try using Hashset 
+let hS = new Set();
+for(let i=0;i<N;i++){
+    hS.add(Ar[i]);
+}
+console.log('HashSet:: ',hS);
+console.log("Distinct elements in Array are :: ",hS.size);
